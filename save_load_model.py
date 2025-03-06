3df02fba57a3db311d50ee0ad5771c6b40531916
Sourav Chandra <39385045+sourav180309@users.noreply.github.com>
2025-03-06 10:30:55 -0800

    Create save_load_model.py

---
+++ save_load_model.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+# Generate dummy data
+x_train = np.random.random((1000, 20))
+y_train = np.random.randint(2, size=(1000, 1))
+x_test = np.random.random((100, 20))
+y_test = np.random.randint(2, size=(100, 1))
+
+# Build a simple model
+model = Sequential()
+model.add(Dense(64, input_dim=20, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+# Save the model
+model.save('/dbfs/tmp/my_model.h5')
+
+# Load the model
+loaded_model = tf.keras.models.load_model('/dbfs/tmp/my_model.h5')
+
+# Evaluate the loaded model
+score = loaded_model.evaluate(x_test, y_test, batch_size=32)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
